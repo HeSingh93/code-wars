0245b946c1ca9a76cf6dc57a74488535279e4b7e
Herman Singh <hermansingh1993@gmail.com>
2019-12-02 10:06:20 +0100

    Initial commit + first kata

---
+++ java/8k-convert-string-to-a-number/src/StringToNumber.java
@@ -0,0 +1,6 @@
+public class StringToNumber {
+    public static int stringToNumber(String str) {
+        int convertedString = Integer.parseInt(str.trim());
+        return convertedString;
+    }
+}
